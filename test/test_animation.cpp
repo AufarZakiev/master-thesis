786e77dffe30eae45df2a6d55943de33979188a3
Safin Ramil <ramilsafnab1996@gmail.com>
2019-03-20 15:08:17 +0300

    leader test added

--- test/test_animation.cpp
+++ test/test_animation.cpp
@@ -88,6 +88,110 @@ TEST(animatedCohesionTest, ShouldPass)
                       std::function(&overallPotential), rg, og, v);
 }
 
+TEST(animatedLeaderTest, ShouldPass)
+{
+  Variables v = Variables();
+  v.setParam("robots_avoidance_distance", 1.5);
+  v.setParam("obstacles_avoidance_distance", 1.0);
+  v.setParam("los_clearance_distance", 0.2);
+  v.setParam("los_clearance_care_distance", 0.4);
+  v.setParam("neighbourhood_distance", 5.0);
+  v.setParam("edge_deletion_distance", 0.1);
+  v.setParam("obstacle_care_distance", 3.0);
+  v.setParam("desired_distance", 3.5);
+  v.setParam("sensing_distance", 10.0);
+  v.setParam("robot_max_speed", 0.1);
+  v.setParam("k1", 10);
+  v.setParam("k2", 10);
+  v.setParam("c1", 0.0);
+  v.setParam("c2", 0.0);
+  v.setParam("c3", 0.0);
+  v.setParam("c4", 10.0);
+
+  ValidatedVariables vv(v);
+
+  Robot r1(Vector_t(5.0, 3.0));
+  Robot r2(Vector_t(10.0, 3.0));
+  Robot r3(Vector_t(7.5, 14.0));
+  RobotGraph rg;
+  auto r1_desc = boost::add_vertex(r1, rg);
+  auto r2_desc = boost::add_vertex(r2, rg);
+  auto r3_desc = boost::add_vertex(r3, rg);
+  boost::add_edge(r1_desc, r2_desc, rg);
+  boost::add_edge(r2_desc, r3_desc, rg);
+
+  ObstacleGraph og;
+
+  ValidatedGraphs vg(rg, og, v);
+
+  r1.setSpeedDirection(Vector_t(-1,-1));
+  r2.setSpeedDirection(getConstrainedDirectedSpeed(r2, vg, vv));
+  r3.setSpeedDirection(getConstrainedDirectedSpeed(r3, vg, vv));
+  rg[r1_desc].setSpeedDirection(Vector_t(-1,-1));
+  rg[r2_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r2_desc], vg, vv));
+  rg[r3_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r3_desc], vg, vv));
+
+  boost::filesystem::create_directories("leaderAnimation");
+  printPlotWithArrows("leaderAnimation/leaderAnimationTest_0_90_1.png", "leaderAnimationTest", 0, 90, 1, { r1, r2, r3 },
+                      std::function(&overallPotential), rg, og, v);
+
+  r1.updatePosition();
+  r2.updatePosition();
+  r3.updatePosition();
+  r2.setSpeedDirection(getConstrainedDirectedSpeed(r2, vg, vv));
+  r3.setSpeedDirection(getConstrainedDirectedSpeed(r3, vg, vv));
+  rg[r1_desc].updatePosition();
+  rg[r2_desc].updatePosition();
+  rg[r3_desc].updatePosition();
+  rg[r2_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r2_desc], vg, vv));
+  rg[r3_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r3_desc], vg, vv));
+
+  printPlotWithArrows("leaderAnimation/leaderAnimationTest_0_90_2.png", "leaderAnimationTest", 0, 90, 1, { r1, r2, r3 },
+                      std::function(&overallPotential), rg, og, v);
+
+  r1.updatePosition();
+  r2.updatePosition();
+  r3.updatePosition();
+  r2.setSpeedDirection(getConstrainedDirectedSpeed(r2, vg, vv));
+  r3.setSpeedDirection(getConstrainedDirectedSpeed(r3, vg, vv));
+  rg[r1_desc].updatePosition();
+  rg[r2_desc].updatePosition();
+  rg[r3_desc].updatePosition();
+  rg[r2_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r2_desc], vg, vv));
+  rg[r3_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r3_desc], vg, vv));
+
+  printPlotWithArrows("leaderAnimation/leaderAnimationTest_0_90_3.png", "leaderAnimationTest", 0, 90, 1, { r1, r2, r3 },
+                      std::function(&overallPotential), rg, og, v);
+
+  r1.updatePosition();
+  r2.updatePosition();
+  r3.updatePosition();
+  r2.setSpeedDirection(getConstrainedDirectedSpeed(r2, vg, vv));
+  r3.setSpeedDirection(getConstrainedDirectedSpeed(r3, vg, vv));
+  rg[r1_desc].updatePosition();
+  rg[r2_desc].updatePosition();
+  rg[r3_desc].updatePosition();
+  rg[r2_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r2_desc], vg, vv));
+  rg[r3_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r3_desc], vg, vv));
+
+  printPlotWithArrows("leaderAnimation/leaderAnimationTest_0_90_4.png", "leaderAnimationTest", 0, 90, 1, { r1, r2, r3 },
+                      std::function(&overallPotential), rg, og, v);
+
+  r1.updatePosition();
+  r2.updatePosition();
+  r3.updatePosition();
+  r2.setSpeedDirection(getConstrainedDirectedSpeed(r2, vg, vv));
+  r3.setSpeedDirection(getConstrainedDirectedSpeed(r3, vg, vv));
+  rg[r1_desc].updatePosition();
+  rg[r2_desc].updatePosition();
+  rg[r3_desc].updatePosition();
+  rg[r2_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r2_desc], vg, vv));
+  rg[r3_desc].setSpeedDirection(getConstrainedDirectedSpeed(rg[r3_desc], vg, vv));
+
+  printPlotWithArrows("leaderAnimation/leaderAnimationTest_0_90_5.png", "leaderAnimationTest", 0, 90, 1, { r1, r2, r3 },
+                      std::function(&overallPotential), rg, og, v);
+}
+
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
